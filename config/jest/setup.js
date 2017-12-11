1cfd78b3b134a73b357b4527c376fa5d4a868d10
david <david.gutierrez@beezy.net>
2017-12-11 02:09:30 +0100

    added enzyme

---
+++ config/jest/setup.js
@@ -0,0 +1,13 @@
+require('core-js/es6/map');
+require('core-js/es6/set');
+require('raf/polyfill');
+const enzyme = require('enzyme');
+const Adapter = require('enzyme-adapter-react-16');
+const toJson = require('enzyme-to-json');
+
+enzyme.configure({ adapter: new Adapter() });
+
+global.shallow = enzyme.shallow;
+global.render = enzyme.render;
+global.mount = enzyme.mount;
+global.toJson = toJson;
